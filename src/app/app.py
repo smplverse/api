176238d8032c733787f795d8c0c15090e690ec36
Piotr <piotr.ostrowski@aol.com>
2022-04-24 03:23:17 +0200

    start on the implementation, add some more test cases

--- src/app/app.py
+++ src/app/app.py
@@ -3,10 +3,14 @@ from werkzeug.sansio.response import Response
 
 from src.app.encode import b64_to_numpy, numpy_to_b64
 from src.inference_package.matcher import Matcher
+from src.eth.init import init
+from hashlib import sha256
 
 app = Flask(__name__)
 
 matcher = Matcher()
+
+_, contract = init()
 
 
 def bad_request(msg: str):
@@ -44,7 +48,31 @@ def assign_smpl():
     if "tokenId" not in request.json:
         bad_request("No tokenId provided")
 
+    tokenId = request.json["tokenId"]
+    sender_address = request.json["address"]
+    image = request.json["image"]
+    _, img_b64 = ",".split(image)
+
+    # TODO check hashlib_img_hash == javascript_img_hash
+    img_hash = sha256(img_b64.encode()).hexdigest()
+
     # verify that address is the owner of a given smpl
+    owner, _, _ = contract.functions.explicitOwnershipOf(tokenId).call()
+    if owner != sender_address:
+        bad_request("address is not the owner of the smpl")
 
     # verify that the hash of image sent is the same as the one in the smart contract
-    return jsonify({"error": None})
+    hash_in_contract: bytes = contract.functions.uploads(tokenId).call()
+    if img_hash != hash_in_contract.hex():
+        bad_request("image hash does not match one in contract")
+
+    # perform the match
+
+    # disclude the smpl from future runs
+    # for the run I think another api of smpls should be up
+    # and running synchronously so that it is nicely FIFO
+
+    # TODO upload the smpl to ipfs here or beforehand? (save time)
+
+    # { CID, confidence, random_three_words?, ...}
+    return jsonify({"something": "fancy"})
