22a3930da821514dd17e067726e4faf5ce95161e
Piotr <piotr.ostrowski@aol.com>
2022-04-24 03:06:38 +0200

    update artifact, add test for checking ownership

--- test/test_eth.py
+++ test/test_eth.py
@@ -5,3 +5,16 @@ def test_inits_right():
     provider, contract = init()
     assert provider.isConnected()
     assert contract.functions.name().call() == "SMPLverse"
+
+
+def test_checks_ownership():
+    _, contract = init()
+    owner_of_first, _, _ = contract.functions.explicitOwnershipOf(0).call()
+    assert owner_of_first == contract.functions.owner().call()
+
+
+def test_checks_hash_uploaded():
+    _, contract = init()
+    upload_hash = contract.all_functions()
+    print(upload_hash)
+    assert 0
