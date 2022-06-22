4753d18c0a2ef664076bbaf22fa039baf2b1c7db
piotrostr <piotr.ostrowski@aol.com>
2022-06-22 14:27:34 +0200

    only use cpu provider

--- src/inference_package/model.py
+++ src/inference_package/model.py
@@ -12,13 +12,6 @@ class Model:
     input_shape: List[int]
     input_name: str
     providers = [
-        ('CUDAExecutionProvider', {
-            'device_id': 0,
-            'arena_extend_strategy': 'kNextPowerOfTwo',
-            'gpu_mem_limit': 2 * 1024 * 1024 * 1024,
-            'cudnn_conv_algo_search': 'EXHAUSTIVE',
-            'do_copy_in_default_stream': True,
-        }),
         ('CPUExecutionProvider', {
             'use_arena': True,
             'arena_extend_strategy': 'kNextPowerOfTwo',
